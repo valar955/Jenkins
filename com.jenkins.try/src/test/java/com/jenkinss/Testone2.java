950bdeb204b8dea0fbed818946eb71d24bc15ae1
ELCOT <ELCOT@Lenovo>
2018-09-19 13:31:01 +0530

    First Commit

---
+++ com.jenkins.try/src/test/java/com/jenkinss/Testone2.java
@@ -0,0 +1,50 @@
+package com.jenkinss;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Testone2 {
+
+@Test	
+	public void sum()
+{
+		
+System.out.println("Sum");
+int a=10;
+int b=20;
+Assert.assertEquals(30, a+b);
+}
+
+@Test	
+public void sub()
+{
+	
+System.out.println("Sub");
+int a=10;
+int b=20;
+Assert.assertEquals(-10, a-b);
+}
+
+@Test	
+public void multi()
+{
+	
+System.out.println("multi");
+int a=10;
+int b=20;
+Assert.assertEquals(200, a*b);
+}
+
+@Test	
+public void div()
+{
+	
+System.out.println("div");
+int a=10;
+int b=20;
+Assert.assertEquals(0, a/b);
+}
+
+}
+
+
